64deb28f0cbb1c959a79d6bcfb444c40a64607d9
BazzS <bazz91@mail.ru>
2021-06-21 12:01:48 +0300

    add client_delay and dinamic_table

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from selenium.common.exceptions import TimeoutException
 
 from .click_page import SearchClick
 from .byclass_attr import SearchHelpClassAttr
+from .client_delay import SearchHelpClientDelay
+from .dinamic_table import SearchDynamicTable
+
 
 def test_click(browser):
     resource = "click"
@@ -21,3 +24,25 @@ def test_classattr(browser):
     page.switch_to_alert_accept()
     button = page.test_button()
     assert button
+
+def test_clientdelay(browser):
+    resource = "clientdelay"
+    page = SearchHelpClientDelay(browser)
+    page.go_to_site(resource)
+
+    page.click_button()
+    button = page.wait_to_text()
+    assert button
+
+def test_dinamictable(browser):
+    resource = "dynamictable"
+    page = SearchDynamicTable(browser)
+    page.go_to_site(resource)
+
+    chrome_cpu = page.chrome_cpu()
+    tasks = page.tasks()
+    list_task = tasks.text.split("\n")
+    chrome_task = [chrome for chrome in list_task if chrome.startswith("Chrome")]
+    chrome_detail_list = chrome_task[0].split()
+    cpu = [cpu for cpu in chrome_detail_list if '%' in cpu]
+    assert chrome_cpu == f'{chrome_detail_list[0]} CPU: {cpu[0]}'
